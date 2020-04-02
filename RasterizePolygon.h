af84d4b8e1d83c6e1867debf895a151bd6e4d9df
Forbes Howington <forbes@forbes-mac-0.vpn.netapp.com>
2020-04-01 21:09:47 -0400

    Added inexact z value calculation, however it diverges. Not sure why

--- RasterizePolygon.h
+++ RasterizePolygon.h
@@ -70,7 +70,7 @@ inline int directionality(const T& p0, const T& p1, const T& p2)
 }
 
 
-unsigned zPos(const unsigned cx, const unsigned bx, const unsigned ax, const unsigned cy, const unsigned by, const unsigned ay, const unsigned cz, const unsigned bz, const unsigned az, const unsigned x, const unsigned y) {
+float zPos(const int cx, const int bx, const int ax, const int cy, const int by, const int ay, const int cz, const int bz, const int az, const int x, const int y) {
   return  az + ((bx - ax) * (cz - az) - (cx - ax) * (bz - az))*(y - ay)/ ((bx-ax) * (cy-ay) - (cx-ax) * (by-ay)) - ((by-ay) * (cz-az) - (cy-ay)*(bz-az))*(x-ax)/((bx-ax)*(cy-ay) - (cx-ax) * (by-ay));
 }
 
@@ -91,22 +91,22 @@ void drawTri(const T& p0, const T& p1, const T& p2,  const unsigned color)
 #endif
 
 
-  const unsigned x0 = p0._x * halfW + halfW;
-  const unsigned x1 = p1._x * halfW + halfW;
-  const unsigned x2 = p2._x * halfW + halfW;
+  const int x0 = p0._x * halfW + halfW;
+  const int x1 = p1._x * halfW + halfW;
+  const int x2 = p2._x * halfW + halfW;
 
-  const unsigned y0 = p0._y * halfH + halfH;
-  const unsigned y1 = p1._y * halfH + halfH;
-  const unsigned y2 = p2._y * halfH + halfH;
+  const int y0 = p0._y * halfH + halfH;
+  const int y1 = p1._y * halfH + halfH;
+  const int y2 = p2._y * halfH + halfH;
 
+  // Prevent from wasting time on polygons that have no area
   if (colinear(x0, x1, x2, y0, y1, y2)) {
     return;
   }
 
-
-  const unsigned z0 = p0._z * 0xFFFF + 0xFFFF;
-  const unsigned z1 = p1._z * 0xFFFF + 0xFFFF;
-  const unsigned z2 = p2._z * 0xFFFF + 0xFFFF;
+  const int z0 = p0._z * 0xFFFFFF + 0xFFFFFF;
+  const int z1 = p1._z * 0xFFFFFF + 0xFFFFFF;
+  const int z2 = p2._z * 0xFFFFFF + 0xFFFFFF;
 
   const int A01 = y0 - y1;
   const int A12 = y1 - y2;
@@ -118,8 +118,8 @@ void drawTri(const T& p0, const T& p1, const T& p2,  const unsigned color)
   const int minX = min3(x0, x1, x2);
   const int minY = min3(y0, y1, y2, true);
 
-  const int maxX = max3(x0, x1, x2, W);
-  const int maxY = max3(y0, y1, y2, H);
+  const int maxX = max3(x0, x1, x2, (int)W);
+  const int maxY = max3(y0, y1, y2, (int)H);
 
   const int bias0 = isTopLeft(p1, p2) ? 0 : -1;
   const int bias1 = isTopLeft(p2, p0) ? 0 : -1;
@@ -129,48 +129,40 @@ void drawTri(const T& p0, const T& p1, const T& p2,  const unsigned color)
   int w1_row = orient2d(x2, x0, minX, y2, y0, minY) + bias1;
   int w2_row = orient2d(x0, x1, minX, y0, y1, minY) + bias2;
 
-  unsigned x, y, xVal, xValInner, numInner, xInner, numOuter;
+  unsigned x, y, z, xVal, xValInner, numInner, xInner, numOuter;
   int w0, w1, w2;
 
 
-  //float zdx = (p0._y *(p2._z - p1._z) + p1._y*(p0._z - p2._z) + p2._y*(p1._z - p2._z)) / (p0._x*(p1._y - p2._y) + p1._x*(p2._y-p0._y) + p2._x*(p0._y-p1._y));
-  //zdx /= (1920/2);
-  //float zdy = (-((-p0._x + p2._x)*(-p0._z + p1._z)) + (-p0._x + p1._x)*(-p0._z + p2._z))/((-p0._x + p1._x)*(p2._y - p0._y) - (-p0._x + p2._x)*(-p0._y + p1._y));
 
+  // These need work...
+  // The exact value of z, and our approximation, diverge over time. Not sure why.
 
+  int zdx = (p2._y *(p0._z - p1._z) + p1._y*(p2._z - p0._z) + p0._y*(p1._z - p0._z)) / (p2._x*(p1._y - p0._y) + p1._x*(p0._y-p2._y) + p0._x*(p2._y-p1._y));
+  int zdy = (-((-x0 + x2)* (-z0 + z1)) + (-x0 + x1)* (-z0 + z2))/(-((-x0 + x2)* (y1 - y0)) + (-x0 + x1) * (-y0 + y2));
+
+  int zOrig = zPos(x0, x1, x2, y0, y1, y2, z0, z1, z2, minX, minY);
 
   for (y = minY; y <= maxY; ++y) {
     w0 = w0_row;
     w1 = w1_row;
     w2 = w2_row;
+    z = zOrig;
 
     numInner = (maxX - minX) / 8;
     numOuter = (maxX - minX) % 8 + 1;
-
-    //float z = w0 * p0._z + w1 * p1._z + w2 * p2._z;
-    //z /= (w0 + w1 + w2);
-
-    //printf("Calculated initial z to be %f, zdx = %f\n", z, zdx);
 
     for (xInner = 0; xInner < numInner; ++xInner) {
       xVal = 8 * xInner + minX;
 
       // We have AVX2, lets take advantage of it!
-      // We break the loop out to enable the compiler to vectorize it
+      // We break the loop out to enable the compiler to vectorize it!
 #pragma clang loop vectorize(enable) interleave(enable)
       for (unsigned x = 0; x < 8; ++x) {
         xValInner = xVal + x;
-
-
-        //float z2 = w0 * p0._z + w1 * p1._z + w2 * p2._z;
-        //z2 /= (w0 + w1 + w2);
-
-        //printf("Z is now %f correct z: %f\n", z, z2);
-
-
         // If p is on or inside all edges, render pixel
-        unsigned z = zPos(x0, x1, x2, y0, y1, y2, z0, z1, z2, xValInner, y);
         if ((w0 | w1 | w2) >= 0){
+          // Uncomment for exact z values
+          z = zPos(x0, x1, x2, y0, y1, y2, z0, z1, z2, xValInner, y);
           if (zbuff[xValInner + W*y] < z) {
             zbuff[xValInner + W*y] = z;
             plot(xValInner, y, color);
@@ -179,7 +171,7 @@ void drawTri(const T& p0, const T& p1, const T& p2,  const unsigned color)
         w0 += A12;
         w1 += A20;
         w2 += A01;
-        //z += zdx;
+        z += zdx;
       }
     }
 
@@ -189,9 +181,9 @@ void drawTri(const T& p0, const T& p1, const T& p2,  const unsigned color)
       xValInner = xVal + x;
 
       // If p is on or inside all edges, render pixel
-      unsigned z = zPos(x0, x1, x2, y0, y1, y2, z0, z1, z2, xValInner, y);
-
       if ((w0 | w1 | w2) >= 0) {
+        // Uncomment for exact z values
+        z = zPos(x0, x1, x2, y0, y1, y2, z0, z1, z2, xValInner, y);
         if (zbuff[xValInner + W*y] < z) {
           zbuff[xValInner + W*y] = z;
           plot(xValInner, y, color);
@@ -200,12 +192,13 @@ void drawTri(const T& p0, const T& p1, const T& p2,  const unsigned color)
       w0 += A12;
       w1 += A20;
       w2 += A01;
-      //z += zdx;
+      z += zdx;
 
     }
     w0_row += B12;
     w1_row += B20;
     w2_row += B01;
+    zOrig += zdy;
   }
 }
 
