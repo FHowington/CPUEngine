a9e7c7b3ace174c774cf636a04e91baa36f03ef5
Forbes Howington <forbes@forbes-mac-0.fios-router.home>
2020-05-10 19:29:54 -0400

    MUCH more efficient Gouraud shading

--- src/shader.h
+++ src/shader.h
@@ -67,70 +67,48 @@ class GourandShader : public Shader {
   void vertexShader(const ModelInstance& m, const face& f, const vertex<float>& light, const short A12, const short A20, const short A01,
                     const short B12, const short B20, const short B01, const float wTotal, int w0, int w1, int w2) override {
     // Change in the texture coordinated for x/y, used for interpolation
-    v0iNorm = (rotateVector(m.position, m.baseModel.getVertexNormal(f._v0)));
-    v1iNorm = (rotateVector(m.position, m.baseModel.getVertexNormal(f._v1)));
-    v2iNorm = (rotateVector(m.position, m.baseModel.getVertexNormal(f._v2)));
+    // OMG I'm a fool, instead just interpolate the intensity
+    vertex<float> v0iNorm = (rotateVector(m.position, m.baseModel.getVertexNormal(f._v0)));
+    vertex<float> v1iNorm = (rotateVector(m.position, m.baseModel.getVertexNormal(f._v1)));
+    vertex<float> v2iNorm = (rotateVector(m.position, m.baseModel.getVertexNormal(f._v2)));
 
-    _xDx = (v0iNorm._x * A12 + v1iNorm._x * A20 + v2iNorm._x * A01) / wTotal;
-    _yDx = (v0iNorm._y * A12 + v1iNorm._y * A20 + v2iNorm._y * A01) / wTotal;
-    _zDx = (v0iNorm._z * A12 + v1iNorm._z * A20 + v2iNorm._z * A01) / wTotal;
+    float light0 = -dot(light, v0iNorm);
+    if (light0 < 0.2) {
+      light0 = 0.2;
+    }
+    float light1 = -dot(light, v1iNorm);
+    if (light1 < 0.2) {
+      light1 = 0.2;
+    }
+    float light2 = -dot(light, v2iNorm);
+    if (light2 < 0.2) {
+      light2 = 0.2;
+    }
 
-    _xDy = (v0iNorm._x * B12 + v1iNorm._x * B20 + v2iNorm._x * B01) / wTotal;
-    _yDy = (v0iNorm._y * B12 + v1iNorm._y * B20 + v2iNorm._y * B01) / wTotal;
-    _zDy = (v0iNorm._z * B12 + v1iNorm._z * B20 + v2iNorm._z * B01) / wTotal;
 
-    _normX = (v0iNorm._x * w0 + v1iNorm._x * w1 + v2iNorm._x * w2) / wTotal;
-    _normY = (v0iNorm._y * w0 + v1iNorm._y * w1 + v2iNorm._y * w2) / wTotal;
-    _normZ = (v0iNorm._z * w0 + v1iNorm._z * w1 + v2iNorm._z * w2) / wTotal;
-    _rowX = _normX;
-    _rowY = _normY;
-    _rowZ = _normZ;
+    _lDx = (light0 * A12 + light1 * A20 + light2 * A01) / wTotal;
+    _lDy = (light0 * B12 + light1 * B20 + light2 * B01) / wTotal;
 
-    _light = light;
+    _light = (light0 * w0 + light1 * w1 + light2 * w2) / wTotal;
+    _lightRow = _light;
   }
   const inline __attribute__((always_inline)) fcolor fragmentShader(const unsigned color) override {
-    vertex<float> normalV(_normX, _normY, _normZ);
-
-    float res = -dot(_light, normalV);
-    if (res < 0.2) {
-      res = 0.2;
-    }
-    return fcolor(color, res);
+    return fcolor(color, _light);
   }
 
   inline __attribute__((always_inline)) void stepXForX(const unsigned step = 1) override {
-    _normX += _xDx * step; _normY += _yDx * step; _normZ += _zDx * step;
+    _light += _lDx * step;
   }
 
   inline __attribute__((always_inline)) void stepYForX(const unsigned step = 1) override
   {
-    _rowX += _xDy * step;
-    _rowY += _yDy * step;
-    _rowZ += _zDy * step;
-    _normX = _rowX;
-    _normY = _rowY;
-    _normZ = _rowZ;
+    _lightRow += _lDy * step;
+    _light = _lightRow;
   }
 
  private:
-  float _normX;
-  float _normY;
-  float _normZ;
-
-  float _xDx;
-  float _yDx;
-  float _zDx;
-
-  float _xDy;
-  float _yDy;
-  float _zDy;
-
-  float _rowX;
-  float _rowY;
-  float _rowZ;
-
-  vertex<float> _light;
-  vertex<float> v0iNorm;
-  vertex<float> v1iNorm;
-  vertex<float> v2iNorm;
+  float _lDx;
+  float _lDy;
+  float _light;
+  float _lightRow;
 };
